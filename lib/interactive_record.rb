afe8d3f83a40eb60eaa5c211d7ae097482497969
Matthew C <m23.cullen@gmail.com>
2019-10-17 15:00:59 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,10 +29,14 @@ class InteractiveRecord
   
   def values_for_insert
     values = []
-    self.class
-  end 
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+    values.join(", ")
+  end
   
   def save
+    
   end 
   
   def self.find_by_name
