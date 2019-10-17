4d6407ed0c1ef44158eef6bd54f934dabec3229a
Matthew C <m23.cullen@gmail.com>
2019-10-17 13:44:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,22 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def table_name_for_insert
+  end 
+  
+  def col_names_for_insert
+  end 
+  
+  def values_for_insert
+  end 
+  
+  def save
+  end 
+  
+  def self.find_by_name
+  end 
+  
+  def self.find_by 
+  end 
   
 end
